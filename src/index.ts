0b18eb33a97ddaa8c3154aec3c65742819d1c656
Anartz Mugika <mugan86@gmail.com>
2017-04-27 10:24:26 +0200

    Define SampleModule export and all elements exports

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { SampleModule } from './sample.module';
+export * from './sample.component';
+export * from './sample.directive';
+export * from './sample.pipe';
+export * from './sample.service';
